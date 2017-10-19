fade4122f1bc8ac485380af0c37eca517939f301
missmakita <mfernanlav@gmail.com>
2017-10-19 07:46:12 -0300

    añade diagrama de flujo y edición menor README.MD

--- app.js
+++ app.js
@@ -2,21 +2,22 @@
 
 do {
 	var response = prompt('Ingresa el N° de tarjeta de crédito:');
-	if (response != '') {
+	if (response !== '') {
     	var cardNum = response;
     	isValidCard(cardNum);
     } else {
     	alert('Ingresa un número válido!');
     }																		  
-} while (response == '');
+} while (response === '');
 
 // Crear función que valida el número de tarjeta ingresado, aplicando el algoritmo de Luhn::
 
 function isValidCard(num) {
 	
-	var result = 0;
 	var pares = [];
 	var impares = [];
+	var result = 0;
+	
 
 // Pasar los números a un array en orden inverso:
 	num = Array.from(num).reverse();
@@ -31,26 +32,31 @@ function isValidCard(num) {
 	}
 
 // Multiplicar los números de las posiciones pares por 2:
-	for (var j = 0; j <= pares.length; j++) {
-		var mult = pares[j]* 2;
-		if (mult >= 10) {
+	var mult = [];
+	
+	for (var j = 0; j < pares.length; j++) {
+		
+		mult = pares[j]* 2;
+		if (mult > 9) {
 			mult = (mult % 10) + (mult / 10);
 		}
 	}
-
+	console.log(pares);
+	console.log(mult);
 // Sumar los números de las posiciones impares al total:	
 	var sum1 = 0;
 	for (var k = 0; k < mult.length; k++) {
 		sum1 += mult[k];
 	}
-
+console.log(sum1);
+	
 	var sum2 = 0;
 	for (var l = 0; l < impares.length; l++) {
 		sum2 += impares[l];
 	}
-
+console.log(sum2);
 	result = sum1 + sum2;
-
+console.log(result);
 // Comprobar si es una tarjeta válida, evaluando el resultado de la suma de los dígitos, si es potencia de 10 es válida, si no, el número de la tarjeta no es válido:
 	if (result % 10 === 0) {
 		console.log(result);
@@ -62,3 +68,5 @@ function isValidCard(num) {
 
 // input 4076115352220310 // result debería darme 40
 // 4287465788753508
+
+// 4916166822592105 (65)
